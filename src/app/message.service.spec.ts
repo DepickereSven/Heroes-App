5e4462f14f4b5b4fc5c9607621266ae25a001e53
SvenDepickere <sven.depickere@student.howest.be>
2019-12-03 14:10:37 +0100

    update to the new way of testing

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,26 +1,23 @@
-import { getTestBed, TestBed } from '@angular/core/testing';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
 
-  let injector: TestBed;
   let messageService: MessageService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MessageService]
-    });
-
-    injector = getTestBed();
-    messageService = injector.get(MessageService);
-  });
+  function create(): MessageService {
+    return new MessageService();
+  }
 
   it('adding a new message: message()', () => {
+    messageService = create();
+
     messageService.add('new message');
     expect(messageService.messages.length).toEqual(1);
   });
 
   it('adding a a lot of new message and clearing', () => {
+    messageService = create();
+
     const distance = 10;
     for (let index = 0; index < distance; index++) {
       messageService.add(`new message ${index}`);
