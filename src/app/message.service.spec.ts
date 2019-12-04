aa982c89b34eda0f70f21181d8918583d4a10a3b
SvenDepickere <sven.depickere@student.howest.be>
2019-12-04 08:36:34 +0100

    Update message.service.spec.ts

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -8,26 +8,30 @@ describe('MessageService', () => {
     return new MessageService();
   }
 
-  it('adding a new message: message()', () => {
-    messageService = create();
+  describe('add()', () => {
+    it('adding a new message: message()', () => {
+      messageService = create();
 
-    messageService.add('new message');
-    expect(messageService.messages.length).toEqual(1);
+      messageService.add('new message');
+      expect(messageService.messages.length).toEqual(1);
+    });
   });
 
-  it('adding a a lot of new message and clearing', () => {
-    messageService = create();
+  describe('clear()', () => {
+    it('adding a a lot of new message and clearing', () => {
+      messageService = create();
 
-    const distance = 10;
-    for (let index = 0; index < distance; index++) {
-      messageService.add(`new message ${index}`);
-    }
+      const distance = 10;
+      for (let index = 0; index < distance; index++) {
+        messageService.add(`new message ${index}`);
+      }
 
-    expect(messageService.messages.length).toEqual(distance);
-    expect(messageService.messages[2]).toEqual('new message 2');
+      expect(messageService.messages.length).toEqual(distance);
+      expect(messageService.messages[2]).toEqual('new message 2');
 
-    messageService.clear();
-    expect(messageService.messages.length).toEqual(0);
+      messageService.clear();
+      expect(messageService.messages.length).toEqual(0);
+    });
   });
 
 });
